c2ec9c9fcb49976de7d924903ec9e13d3705bd6a
DeAnna Boiani <deannaboiani@gmail.com>
2021-09-23 16:12:31 -0700

    add randomizer adn new variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
 //TODO: write 4 arrays: one for lowercase, one upper, and special
-var lowerAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var capAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'V'];
-var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var cap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'V'];
+var num = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var symbol = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '-', '_', '='];
-
-
 var include = [];
+var random = [];
+var pwArr = [];
 
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
 //TODO: ask the user how many characters they want in the password -- use a prompt which will be set to a var -- make sure the infromation is correct -- number -- 8+ or 128-
 
-var lengthChoice = window.prompt("How long do you want your password to be? Answer MUST be between 8 and 128");
+var lengthChoice = parseInt(prompt("How long do you want your password to be? Answer MUST be between 8 and 128"));
+console.log(lengthChoice)
 if (lengthChoice > 128 || lengthChoice < 8) {
 alert("number is not in the parameters");
-} else {
-console.log(lengthChoice);
+generatePassword();
 }
 
   
 // THEN I am presented with a series of prompts for password criteria
 
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
 
 //TODO:  write a series of 4 confirms --set these to be a var -- to check if the user wants
 var capChoice = confirm("Do you want uppercase letters in your password? Press Okay for yes, or Cancel for no.");
-if (capChoice === true) {
+if (capChoice) {
   //TODO: check that the prompts true or false --using a var for this -- add the associated arrays to a "holding array" upperConfirm -- true -- upperCase[] -- add to holding []
-  include.push('capAlpha')
+  
+  for (let i = 0; i < cap.length; i++) {
+    include.push(cap[i])
+  }
+  
   console.log("Wants uppercase letters");
 } else {
   console.log("No uppercase letters");
 }
 
 var lowerChoice = confirm("Do you want any lowercase letters in your password? Press Okay for yes, or Cancel for no.")
-if (lowerChoice === true) {
-  include.push('lowerAlpha');
+if (lowerChoice) {
+  for (let i = 0; i < lower.length; i++) {
+    include.push(lower[i])
+  }
   console.log("Wants lowercase letters");
 } else {
   console.log("No lowercase letters");
 }
 
 var numChoice = confirm("Do you want any numbers in your password? Press Okay for yes, or Cancel for no.")
-if (numChoice === true) {
-  include.push('num');
+if (numChoice) {
+  for (let i = 0; i < num.length; i++) {
+    include.push(num[i])
+  }
+  
   console.log("Wants numbers");
 
 } else {
@@ -56,23 +65,30 @@ if (numChoice === true) {
 // WHEN asked for character types to include in the password
 
 var specialChoice = confirm("Do you want any special characters in your password? Press Okay for yes, or Cancel for no.")
-if (specialChoice === true) {
-  include.push('symbol');
+if (specialChoice) {
+  for (let i = 0; i < symbol.length; i++) {
+    include.push(symbol[i])
+  }
   console.log("Wants special characters");
 } else {
   console.log("No special characters");
 }
-
+console.log(include)
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 
-
-
-function generatePassword() {
-
-  return "password"
+for (let i = 0; i < include.length; i++) {
+   random.push(include[Math.floor(Math.random() * include.length)])
+  
 }
+console.log(random)
+ for (let i = 0; i < lengthChoice; i++) {
+   pwArr.push(random[Math.floor(Math.random() * random.length)])
+   
+ }
 
-// // Write password to the #password input
+console.log(pwArr)
+  return pwArr.join("");
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -80,9 +96,12 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// // Write password to the #password input
+
 
 // // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
+
 
 
 // GIVEN I need a new, secure password
